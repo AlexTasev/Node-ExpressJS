8afd70e1a56b373b733c8533628998b4c131741a
Aleksandar Tasev <40795644+AlexTasev@users.noreply.github.com>
2019-02-09 18:05:00 +0200

    Add files via upload

---
+++ ExamPrep/01_Messanger/01_Messanger/models/Thread.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+let threadSchema = new mongoose.Schema({
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }],
+    dataCreated: {
+        type: mongoose.Schema.Types.Date,
+        default: Date.now
+    }
+});
+
+const Thread = mongoose.model('Thread', threadSchema);
+
+module.exports = Thread;
